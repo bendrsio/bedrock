babc66fddc41161fe27c3119ea07f996cc8f5efa
Felix Stavonhagen <felixstavonhagen@gmail.com>
2025-10-22 11:26:07 +0200

    Add application icons and update forge config
    Added icon.icns, icon.ico, and icon.png to src/assets and configured Electron Forge to use these icons for packaging. Also updated package.json scripts and improved formatting in forge.config.ts.

--- forge.config.ts
+++ forge.config.ts
@@ -12,46 +12,47 @@ import { mainConfig } from "./webpack.main.config";
 import { rendererConfig } from "./webpack.renderer.config";
 
 const config: ForgeConfig = {
-  packagerConfig: {
-    asar: true,
-  },
-  rebuildConfig: {},
-  makers: [
-    new MakerSquirrel({}),
-    new MakerZIP({}, ["darwin"]),
-    new MakerRpm({}),
-    new MakerDeb({}),
-  ],
-  plugins: [
-    new AutoUnpackNativesPlugin({}),
-    new WebpackPlugin({
-      mainConfig,
-      renderer: {
-        config: rendererConfig,
-        entryPoints: [
-          {
-            html: "./src/renderer/index.html",
-            js: "./src/renderer/renderer.ts",
-            name: "main_window",
-            preload: {
-              js: "./src/main/preload.ts",
+    packagerConfig: {
+        asar: true,
+        icon: "./src/assets/icon",
+    },
+    rebuildConfig: {},
+    makers: [
+        new MakerSquirrel({}),
+        new MakerZIP({}, ["darwin"]),
+        new MakerRpm({}),
+        new MakerDeb({}),
+    ],
+    plugins: [
+        new AutoUnpackNativesPlugin({}),
+        new WebpackPlugin({
+            mainConfig,
+            renderer: {
+                config: rendererConfig,
+                entryPoints: [
+                    {
+                        html: "./src/renderer/index.html",
+                        js: "./src/renderer/renderer.ts",
+                        name: "main_window",
+                        preload: {
+                            js: "./src/main/preload.ts",
+                        },
+                    },
+                ],
             },
-          },
-        ],
-      },
-    }),
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
-    new FusesPlugin({
-      version: FuseVersion.V1,
-      [FuseV1Options.RunAsNode]: false,
-      [FuseV1Options.EnableCookieEncryption]: true,
-      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
-      [FuseV1Options.EnableNodeCliInspectArguments]: false,
-      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
-      [FuseV1Options.OnlyLoadAppFromAsar]: true,
-    }),
-  ],
+        }),
+        // Fuses are used to enable/disable various Electron functionality
+        // at package time, before code signing the application
+        new FusesPlugin({
+            version: FuseVersion.V1,
+            [FuseV1Options.RunAsNode]: false,
+            [FuseV1Options.EnableCookieEncryption]: true,
+            [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+            [FuseV1Options.EnableNodeCliInspectArguments]: false,
+            [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+            [FuseV1Options.OnlyLoadAppFromAsar]: true,
+        }),
+    ],
 };
 
 export default config;
